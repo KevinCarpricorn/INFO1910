aafba647ab98cdcc7fd1eef299007db04c733c3f
Kevin <zhengyuxiang8888@gmail.com>
2021-04-24 17:59:16 +0800

    final versoin

--- C coding/workspace/py_functions.py
+++ C coding/workspace/py_functions.py
@@ -9,7 +9,8 @@ def py_r_psnr(image1, image2):
     for line in image2:
         red2.append(line[0])        # all the values in red channel of image2
     len_img = len(red1)
-    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), red1, red2)))) / len_img) * 1000000000000000        # mean squared error(MSE)
+    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), red1, red2)))) / len_img) \
+          * 1000000000000000        # mean squared error(MSE)
     if mse == 0:
         return 0.0
     else:
@@ -25,7 +26,8 @@ def py_g_psnr(image1, image2):
     for line in image2:
         green2.append(line[1])        # all the values in green channel of image2
     len_img = len(green1)
-    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), green1, green2)))) / len_img) * 1000000000000000        # mean squared error(MSE)
+    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), green1, green2)))) / len_img)\
+          * 1000000000000000        # mean squared error(MSE)
     if mse == 0:
         return 0.0
     else:
@@ -41,7 +43,8 @@ def py_b_psnr(image1, image2):
     for line in image2:
         blue2.append(line[2])        # all the values in blue channel of image2
     len_img = len(blue1)
-    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), blue1, blue2)))) / len_img) * 1000000000000000         # mean squared error(MSE)
+    mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), blue1, blue2)))) / len_img) \
+          * 1000000000000000         # mean squared error(MSE)
     if mse == 0:
         return 0.0
     else:
@@ -65,7 +68,10 @@ def py_total_psnr(image1, image2):
             blue2.append(line[2])
         len_img = len(red1)
         # mean squared error(MSE) for coloured images
-        mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), red1, red2))) + sum(list(map(lambda x, y: (x-y) * (x-y), green1, green2))) + sum(list(map(lambda x, y: (x-y) * (x-y), blue1, blue2)))) / (3 * len_img)) * 1000000000000000 
+        mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), red1, red2)))
+                + sum(list(map(lambda x, y: (x-y) * (x-y), green1, green2)))
+                + sum(list(map(lambda x, y: (x-y) * (x-y), blue1, blue2)))) / (3 * len_img)) \
+              * 1000000000000000
         if mse == 0:
             return 0.0
         else:
@@ -78,11 +84,10 @@ def py_total_psnr(image1, image2):
         for line in image2:
             pixel2.append(line[0])
         len_img = len(pixel1)
-        mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), pixel1, pixel2)))) / len_img) * 1000000000000000 
+        mse = ((sum(list(map(lambda x, y: (x-y) * (x-y), pixel1, pixel2)))) / len_img)\
+              * 1000000000000000
         if mse == 0:
             return 0.0
         else:
             psnr = 10 * math.log10((255 * 255 * 1000000000000000 / mse))
             return psnr
-
-
